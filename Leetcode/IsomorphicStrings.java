dc7556a1c6fbfb7b636d47879258133d598b5953
KaranNagesh <Karannagesh86@gmail.com>
2026-04-28 22:42:14 +0530

    Day - 21 -> 28/04/2026

---
+++ Leetcode/IsomorphicStrings.java
@@ -0,0 +1,20 @@
+class Solution {
+    // 205. Isomorphic Strings
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character,Character> map = new HashMap<>();
+        for(int i = 0 ; i < s.length() ; i++){
+            char ss = s.charAt(i);
+            char tt = t.charAt(i);
+            if((map.containsKey(ss) && map.get(ss) != tt) || 
+                (!map.containsKey(ss) && map.values().contains(tt)
+            )){
+                return false;
+            }
+            map.put(ss,tt);
+
+        }
+        return true;
+
+        
+    }
+}
