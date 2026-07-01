e972f8d6d5e6a420fd5b637004f7e3efca7da6b7
KaranNagesh <Karannagesh86@gmail.com>
2026-07-01 14:39:29 +0530

    Day - 34 -> 01/07/2026

---
+++ Leetcode/KthMissingElement.java
@@ -0,0 +1,13 @@
+class Solution {
+    //1539. Kth Missing Positive Number
+    public int findKthPositive(int[] arr, int k) {
+        int low = 0 , high = arr.length - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            int missing = arr[mid] - (mid + 1);
+            if(missing < k) low = mid + 1;
+            else high = mid - 1;
+        }
+        return low + k ;
+    }
+}
