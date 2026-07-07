616fa65a04684f0c9b536741dcc8fd058471f26a
KaranNagesh <Karannagesh86@gmail.com>
2026-07-07 14:01:15 +0530

    Day - 40 -> 07/07/2026

---
+++ Leetcode/SearchInMatrix2.java
@@ -0,0 +1,13 @@
+class Solution {
+    //240. Search a 2D Matrix II
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length  , n = matrix[0].length;
+        int i = 0, j = n - 1;
+        while(i < m && j >= 0){
+            if(matrix[i][j] == target) return true;
+            if(matrix[i][j] < target) i++;
+            else j--;
+        }
+        return false;
+    }
+}
