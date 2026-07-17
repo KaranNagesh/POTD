3c3d80a2eaa9cc16685d11f6d44183058d620af6
KaranNagesh <Karannagesh86@gmail.com>
2026-07-17 19:18:59 +0530

    Day - 50 -> 17/07/2026

---
+++ Leetcode/Reversepairs.java
@@ -0,0 +1,52 @@
+class Solution {
+    //493. Reverse Pairs
+    static int count;
+    public int reversePairs(int[] nums) {
+        count = 0;
+        mergesort(nums);
+        return count;
+    }
+    public void inversion(int[] a,int[] b){
+        int i = 0 , j = 0;
+        while(i < a.length && j < b.length){
+            if((long) a[i] > (long) 2 * b[j]){
+                count += (a.length - i);
+                j++;
+            }
+            else i++;
+        }
+    }
+    public void mergesort(int[] nums){
+        int n = nums.length;
+        if(n == 1){
+            return;
+        }
+        int[] a = new int[n/2];
+        int[] b = new int[n - n/2];
+        int idx = 0;
+        for(int i = 0 ; i < a.length ;i++){
+            a[i] = nums[idx];
+            idx++;
+        }
+        for(int i = 0; i < b.length;i++){
+            b[i] = nums[idx];
+            idx++;
+        }
+        mergesort(a); mergesort(b);
+        inversion(a,b);
+        merge(a,b,nums);
+        a = null; b = null;
+    }
+    public void merge(int[] a ,int[] b , int[] arr){
+        int i = 0 , j = 0 , k = 0;
+        while(i < a.length && j < b.length){
+            if(a[i] <= b[j]){
+                arr[k++] = a[i++];
+            }else{
+                arr[k++] = b[j++];
+            }
+        }
+        while(i < a.length) arr[k++] = a[i++];
+        while(j < b.length) arr[k++] = b[j++];
+    }
+}
